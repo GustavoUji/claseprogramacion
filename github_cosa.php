5696113b220e3abdb89afdf408d82a8e3a0a2932
Andrés <34494904+and-y93@users.noreply.github.com>
2018-05-23 15:43:25 +0200

    Update github_cosa.php

--- github_cosa.php
+++ github_cosa.php
@@ -9,6 +9,11 @@ class github_cosa extends CI_Controller {
       echo "<h2>Hola a todos. Soy <h1>Gustavo</h1>.</h2>";	
     }
     
+    public function andres()  {        	
+    	// helpers y libraries
+      echo "<h2>Hola a todos. Soy <h1>Andrés</h1>.</h2>";	
+    }
+    
     public function alumno() {     	
     	// helpers y libraries
     
