b450d7b2d5c3c0c585827e5b3041a881c8f68f54
julialujan666 <39529606+julialujan666@users.noreply.github.com>
2018-05-23 15:49:39 +0200

    Update github_cosa.php

--- github_cosa.php
+++ github_cosa.php
@@ -9,8 +9,9 @@ class github_cosa extends CI_Controller {
       echo "<h2>Hola a todos. Soy <h1>Gustavo</h1>.</h2>";	
     }
     
-    public function alumno() {     	
-    	// helpers y libraries
+    public function julia() {   
+        // helpers y libraries
+    	   echo "<h2>Hola a todos. Soy <h1>Julia</h1>.</h2>";	
     
     }
 
