6bfde9a9a3f723fd0b6eed1c01a492ecaa189a47
nicolayrojo <34812446+nicolayrojo@users.noreply.github.com>
2018-05-23 15:45:41 +0200

    Update github_cosa.php

--- github_cosa.php
+++ github_cosa.php
@@ -9,9 +9,9 @@ class github_cosa extends CI_Controller {
       echo "<h2>Hola a todos. Soy <h1>Gustavo</h1>.</h2>";	
     }
     
-    public function alumno() {     	
+    public function nicolay() {     	
     	// helpers y libraries
-    
+     echo "<h2>Hola a todos. Soy <h1>Nicolay</h1>.</h2>";	
     }
 
 }
