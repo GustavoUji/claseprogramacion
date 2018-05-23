ab6f67dde0b6c21d0496b04abec09661da828216
RubenReguera <39529548+RubenReguera@users.noreply.github.com>
2018-05-23 15:47:24 +0200

    Add files via upload
    Añadida función ruben

---
+++ github_cosa.php
@@ -0,0 +1,19 @@
+<?php 
+class github_cosa extends CI_Controller {
+    public function index()  {        	
+    	// helpers y libraries
+    	echo "<h2>Hola a todos.</h2>";
+    }
+    public function gustavo()  {        	
+    	// helpers y libraries
+      echo "<h2>Hola a todos. Soy <h1>Gustavo</h1>.</h2>";	
+    }
+    
+    public function ruben() {     	
+    	// helpers y libraries
+    	echo "<p>Función de Rubén</p>";
+    
+    }
+
+}
+?>
